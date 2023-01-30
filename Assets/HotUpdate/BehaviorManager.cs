bdf073bec07687b579ab2471321c56b5ebb93ac0
YuShunJie <yushj@yoozoo.com>
2023-01-30 11:21:56 +0800

    崩溃问题

---
+++ Assets/HotUpdate/BehaviorManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+//[AddComponentMenu("Add Behavior Manager")]
+public class BehaviorManager : MonoBehaviour
+{
+    static public BehaviorManager instance;
+
+    //private static MethodInfo playMakerStopMethod = null;
+    //private static MethodInfo PlayMakerStopMethod => playMakerStopMethod;
+
+    /// <summary>
+    /// Reset the static variables for domain reloading.
+    /// </summary>
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+    private static void DomainReset()
+    {
+        // 这句必须有，注掉不会崩溃
+        instance = null;
+
+        Debug.Log("Are U OK?");
+    }
+}
